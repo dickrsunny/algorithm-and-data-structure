8f206c690029f363bb3f94552ad77cb7d380b42d
Dickr <1782463637@qq.com>
2018-10-09 18:47:00 +0800

    添加新解法

--- speak_opposite_words/main.cpp
+++ speak_opposite_words/main.cpp
@@ -1,38 +1,22 @@
-#include <cstdio>
-#include <cstring>
+#include <iostream>
+#include <stack>
+
+using namespace std;
 
 
 int main() {
+    stack<string> st;
+    string s;
 
-//    gets(string);
-//
+    while (cin >> s) st.push(s);
 
+    cout << st.top();
+    st.pop();
 
-    char string[90];
-    int i = 0;
-
-    while (scanf("%c", &string[i]) != EOF) {
-        i++;
+    while (!st.empty()) {
+        cout << " " << st.top();
+        st.pop();
     }
-
-    char matrix[90][90];
-    int j = 0, k = 0;
-
-    for (int i = 0; i < strlen(string); i++) {
-        if (string[i] == 32) {
-            j = 0;
-            k++;
-        } else {
-            matrix[k][j] = string[i];
-            j++;
-        }
-    }
-
-    for (int j = k; j > 0; j--) {
-        printf("%s ", matrix[j]);
-    }
-
-    printf("%s", matrix[0]);
 
     return 0;
-    }
+}
